c3fc79d0244fa148010a3e97dee7b5e53fb93c2c
Abdillah <abdillah1453@gmail.com>
2022-03-22 09:27:29 +0700

    fitur user selesai

--- src/server.js
+++ src/server.js
@@ -1,12 +1,19 @@
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
-const notes = require('./api/albums');
+const songs = require('./api/albums');
 const PostgretService = require('./service/postgres/AlbumsService');
 const PostgresValidator = require('./validator/postgres');
 
+// users
+const users = require('./api/users');
+const UsersService = require('./service/postgres/UsersService');
+const UsersValidator = require('./validator/users');
+
 const init = async () => {
   const postgresService = new PostgretService();
+  const usersService = new UsersService();
+
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -17,13 +24,22 @@ const init = async () => {
     },
   });
 
-  await server.register({
-    plugin: notes,
-    options: {
-      service: postgresService,
-      validator: PostgresValidator,
+  await server.register([
+    {
+      plugin: songs,
+      options: {
+        service: postgresService,
+        validator: PostgresValidator,
+      },
     },
-  });
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
+      },
+    },
+  ]);
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
